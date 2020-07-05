3b408f98c1f44faa2ecbfd3033143eb7f9e72a0f
Nikhil Naidu <nikhilnaidun9n@gmail.com>
2020-07-05 19:07:10 +0530

    Assets Added

--- lib/landing.dart
+++ lib/landing.dart
@@ -3,24 +3,67 @@ import 'package:flutter/material.dart';
 class LandingPage extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return Container(
-      child: SingleChildScrollView(
-        child: Column(
-          children: [
-            productPage('1.png', context),
-            productPage('2.png', context),
-            productPage('3.png', context),
-            productPage('4.png', context),
-            productPage('5.png', context),
-            productPage('6.png', context),
-          ],
-        ),
-      ),
-    );
+    return LayoutBuilder(builder: (context, constraints) {
+      if (constraints.maxWidth > 800) {
+        return Container(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                productPage(
+                    'https://raw.githubusercontent.com/NikhilNaidu9/skimmer-product-page/master/assets/1.png',
+                    context),
+                productPage(
+                    'https://raw.githubusercontent.com/NikhilNaidu9/skimmer-product-page/master/assets/2.png',
+                    context),
+                productPage(
+                    'https://raw.githubusercontent.com/NikhilNaidu9/skimmer-product-page/master/assets/3.png',
+                    context),
+                productPage(
+                    'https://raw.githubusercontent.com/NikhilNaidu9/skimmer-product-page/master/assets/4.png',
+                    context),
+                productPage(
+                    'https://raw.githubusercontent.com/NikhilNaidu9/skimmer-product-page/master/assets/5.png',
+                    context),
+                productPage(
+                    'https://raw.githubusercontent.com/NikhilNaidu9/skimmer-product-page/master/assets/6.png',
+                    context),
+              ],
+            ),
+          ),
+        );
+      } else {
+        return Container(
+          child: SingleChildScrollView(
+            child: Column(
+              children: [
+                productPage(
+                    'https://raw.githubusercontent.com/NikhilNaidu9/skimmer-product-page/master/assets/1.png',
+                    context),
+                productPage(
+                    'https://raw.githubusercontent.com/NikhilNaidu9/skimmer-product-page/master/assets/2.png',
+                    context),
+                productPage(
+                    'https://raw.githubusercontent.com/NikhilNaidu9/skimmer-product-page/master/assets/3.png',
+                    context),
+                productPage(
+                    'https://raw.githubusercontent.com/NikhilNaidu9/skimmer-product-page/master/assets/4.png',
+                    context),
+                productPage(
+                    'https://raw.githubusercontent.com/NikhilNaidu9/skimmer-product-page/master/assets/5.png',
+                    context),
+                productPage(
+                    'https://raw.githubusercontent.com/NikhilNaidu9/skimmer-product-page/master/assets/6.png',
+                    context),
+              ],
+            ),
+          ),
+        );
+      }
+    });
   }
 
   Widget productPage(String filePath, BuildContext context) {
-    double width = MediaQuery.of(context).size.width;
+    double width = MediaQuery.of(context).size.width / 1.25;
     double height = MediaQuery.of(context).size.height;
 
     return Container(
@@ -28,7 +71,7 @@ class LandingPage extends StatelessWidget {
       height: height,
       decoration: BoxDecoration(
           image:
-              DecorationImage(image: AssetImage(filePath), fit: BoxFit.cover)),
+              DecorationImage(image: NetworkImage(filePath), fit: BoxFit.fill)),
     );
   }
 }
